0ffe97d63ebc27dc3b112cfd7d4e703da34a9860
Satrio Nugroho <bukanbegobiasa@gmail.com>
2017-09-05 23:10:18 +0700

    maybe fix error with automatic install

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-export VIM_REPO="git@github.com:satrionugroho/vim-config.git"
+export VIM_REPO="https://github.com/satrionugroho/vim-config.git"
 export VIM_INSTALL_DIR="${HOME}/.vim-config"
 export VIM_DIR="${HOME}/.vim"
+export NEOBUNDLE_URL="https://github.com/Shougo/neobundle.vim"
 
 # move to tmp when error on home
 cd /tmp
@@ -16,6 +17,12 @@ if [ -d $VIM_INSTALL_DIR ]; then
 fi
 
 git clone $VIM_REPO $VIM_INSTALL_DIR
+git clone $NEOBUNDLE_URL $VIM_INSTALL_DIR/bundle/neobundle.vim
 cd $VIM_INSTALL_DIR
 
-rake
+# create symlinks
+ln -sf "${VIM_INSTALL_DIR}/.vim" $VIM_DIR
+ln -sf "${VIM_INSTALL_DIR}/.vimrc" ${HOME}/.vimrc
+ln -sf "${VIM_INSTALL_DIR}/.gvimrc" ${HOME}/.gvimrc
+
+/usr/bin/vim +NeoBundleInstal +qall
